e6f0271f5a9ed95968eaf57438f691198ef94e1a
FindMalek <malekgarahellalbus@gmail.com>
2024-07-20 20:07:15 +0100

    style(about): ordering & layout of bento

--- components/app/marketing-about.tsx
+++ components/app/marketing-about.tsx
@@ -1,11 +1,116 @@
 import React from "react"
 
+import { AnimatedBeam } from "@/components/fancy/animated-beam"
+import { BentoCard, BentoGrid } from "@/components/fancy/bento-grid"
 import { GradientCircle } from "@/components/fancy/gradient-circle"
+import { Icons } from "@/components/shared/icons"
+
+const features = [
+  {
+    Icon: Icons.lock,
+    name: "Secure Authentication",
+    description: "Ready-to-use, secure user authentication with Next Auth.",
+    href: "#",
+    cta: "Learn more",
+    className: "col-span-3 lg:col-span-1",
+    background: (
+      <>
+        Implement robust user login, registration, and password management
+        out-of-the-box.
+      </>
+    ),
+  },
+  {
+    Icon: Icons.graph,
+    name: "Insightful Analytics",
+    description: "Monitor your app's usage with LogSnag.",
+    className: "col-span-3 lg:col-span-2",
+    href: "#",
+    cta: "Learn more",
+    background: (
+      <>
+        Track user interactions and gather valuable insights to improve your
+        application.
+      </>
+    ),
+  },
+  {
+    Icon: Icons.mail,
+    name: "Email Integration",
+    description: "Seamless email sending with Nodemailer and Gmail SMTP.",
+    href: "#",
+    cta: "Learn more",
+    className: "col-span-3 lg:col-span-1",
+    background: (
+      <>Send transactional emails, notifications, and more without hassle.</>
+    ),
+  },
+  {
+    Icon: Icons.puzzle,
+    name: "Ready Components",
+    description: "Variety of pre-built pages and UI components.",
+    href: "#",
+    cta: "Learn more",
+    className: "col-span-3 lg:col-span-2",
+    background: (
+      <>
+        Speed up your development with customizable, pre-designed pages and
+        components.
+      </>
+    ),
+  },
+  {
+    Icon: Icons.globe,
+    name: "Global Reach",
+    description:
+      "Built-in support for multiple languages with Next Internationalization.",
+    className: "col-span-3 lg:col-span-1",
+    href: "#",
+    cta: "Learn more",
+    background: (
+      <>
+        Easily add and manage translations to make your app accessible to a
+        global audience.
+      </>
+    ),
+  },
+
+  {
+    Icon: Icons.databaseZap,
+    name: "Database Management",
+    description: "Efficient database handling with Prisma ORM.",
+    className: "col-span-3 lg:col-span-1",
+    href: "#",
+    cta: "Learn more",
+    background: (
+      <>Simplify your database interactions with powerful Prisma tools. </>
+    ),
+  },
+  {
+    Icon: Icons.blog,
+    name: "Blog Pages",
+    description:
+      "Create, edit, and manage blog posts with static Markdown pages.",
+    className: "col-span-3 lg:col-span-1",
+    href: "#",
+    cta: "Learn more",
+    background: (
+      <>
+        Easily add and manage content with a built-in blogging system, complete
+        with Markdown support for rich text formatting.
+      </>
+    ),
+  },
+]
 
 export function MarketingAbout() {
   return (
     <GradientCircle>
-      Bento grid of all of the features findplate would have
+      <BentoGrid className="container">
+        {features.map((feature, idx) => (
+          <BentoCard key={idx} {...feature} />
+        ))}
+      </BentoGrid>
     </GradientCircle>
   )
 }
--- components/fancy/bento-grid.tsx
+++ components/fancy/bento-grid.tsx
@@ -45,16 +45,14 @@ const BentoCard = ({
     key={name}
     className={cn(
       "group relative col-span-3 flex flex-col justify-between overflow-hidden rounded-xl",
-      // light styles
       "bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]",
-      // dark styles
-      "transform-gpu dark:bg-black dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]",
+      "dark:bg-secondary/20 transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]",
       className
     )}
   >
     <div>{background}</div>
     <div className="pointer-events-none z-10 flex transform-gpu flex-col gap-1 p-6 transition-all duration-300 group-hover:-translate-y-10">
-      <Icon className="size-12 origin-left transform-gpu text-neutral-700 transition-all duration-300 ease-in-out group-hover:scale-75" />
+      <Icon className="group-hover:text-primary size-12 origin-left transform-gpu text-neutral-700 transition-all duration-300 ease-in-out group-hover:scale-75" />
       <h3 className="text-xl font-semibold text-neutral-700 dark:text-neutral-300">
         {name}
       </h3>
