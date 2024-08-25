bc984de136bb23e7fbcbcc049742f88c8ba89446
FindMalek <malekgarahellalbus@gmail.com>
2024-08-25 18:43:53 +0100

    feat: added services pages

---
+++ app/(marketing)/(services)/services/commercial-vehicles/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function CommercialVehicles
+() {
+  return <div>CommercialVehicles
+    
+  </div>;
+}
---
+++ app/(marketing)/(services)/services/long-term-rental/page.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function LongTermRental() {
+  return <div>LongTermRental</div>
+}
